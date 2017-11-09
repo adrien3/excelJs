35d2584b65f8f83e9559daaf156ccbe78d3fa05d
Joao Veiga <joao.rodriguesveiga@altran.com>
2017-11-09 10:03:37 +0100

    refactoring

--- index.js
+++ index.js
@@ -3,27 +3,43 @@ var Excel = require('exceljs');
 var workbook = new Excel.Workbook();
 
 workbook.xlsx.readFile('ENGIE_TEMPLATE_manyPages.xlsx')
-    .then(function() {
-	 var worksheet = workbook.getWorksheet(3);
+	.then(function () {
+		var worksheet 	= workbook.getWorksheet(3);
+		var index 		= workbook.getWorksheet(4);
 
-	 if(worksheet){
-		// Add column
-		var column = worksheet.getColumn("W");
-		var id_new_column = 23;
-		column._number = id_new_column;
-		
+		if (worksheet) {
+			// Add column
+			var column 			= worksheet.getColumn("W");
+			var id_new_column 	= 22;
+			var row 			= worksheet.getRow(2);
+			var formule 		= ['index!$A$2:$A$3'];
+			var begin_data 		= 3;
+			var n_col_add 		= 40;
+			var n_row_add 		= 25;
 
-		//Add info ligne.
-		// iterate over all current cells in this column
-		var row = worksheet.getRow(2);
-		row.getCell(id_new_column).value = "Nom du config";
 
-		if(worksheet._columns.length >= 22){
-			worksheet._columns[22] = column;
-		}else{
-			worksheet._columns.push(column);
+			for (let i = 1; i <= n_col_add; i++) {
+				id_new_column = id_new_column + 1;
+				column._number = id_new_column;
+				row.getCell(id_new_column).value = "Nom du config " + i;
+
+				for ( let j = begin_data; j < n_row_add; j++) {
+					let newRow = worksheet.getRow(j);
+					newRow.getCell(id_new_column).dataValidation = {
+						type: 'list',
+						allowBlank: true,
+						formulae: formule
+					};
+				}
+
+				if (worksheet._columns.length >= id_new_column) {
+					worksheet._columns[id_new_column] = column;
+				} else {
+					worksheet._columns.push(column);
+				}
+			}
+
 		}
-	 }
 
-	return workbook.xlsx.writeFile('new.xlsx');
-});
+		return workbook.xlsx.writeFile('new.xlsx');
+	});
